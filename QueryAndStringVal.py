bf8a7a8aed8c509a20ada6fbd0e8fdaac71e0575
Timothy <timothyp2099@gmail.com>
2025-04-17 17:29:38 -0700

    Query and string Validation partial 2

--- QueryAndStringVal.py
+++ QueryAndStringVal.py
@@ -5,8 +5,30 @@ app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(q: Annotated[str | None, Query(max_length=50)] = None):
+async def read_items(
+    # ^ meand starts with the following characters
+    # fixedquery has the exact value fixedquery
+    # $ meands ends her with no following characters
+    q: Annotated[
+        str | None, Query(min_length=3, max_length=50, pattern="^fixedquery$") #could use regex instead of pattern
+    ] = None):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
     return results
+
+# Old method using Query
+'''@app.get("/items/")
+async def read_items(q: str | None = Query(default=None, max_length=50)):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results'''
+
+# default values other than None 
+'''@app.get("/items/")
+async def read_items(q: Annotated[str, Query(min_length=3)] = "fixedquery"):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results'''
