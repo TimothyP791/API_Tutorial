d370967132c99830cd3177365a1865005a99c00f
Timothy <timothyp2099@gmail.com>
2025-04-27 12:03:23 -0700

    Declare Request Example Data completed

--- Declare_Request_Example_Data.py
+++ Declare_Request_Example_Data.py
@@ -46,8 +46,9 @@ class Item(BaseModel):
     tax: float | None = None
 
 
-@app.put("/items/{item_id}")
+'''@app.put("/items/{item_id}")
 async def update_item(
+    *,
     item_id: int,
     item: Annotated[
         Item,
@@ -72,4 +73,48 @@ async def update_item(
     ],
 ):
     results = {"item_id": item_id, "item": item}
+    return results'''
+
+#Work around for the above example not showing multiple examples
+#Each example is a dictionary with the same keys as the model
+#Each example can have a summary description, value, and externalValue
+@app.put("/items/{item_id}")
+async def update_item(
+    *,
+    item_id: int,
+    item: Annotated[
+        Item,
+        Body(
+            openapi_examples={
+                "normal": {
+                    "summary": "A normal example",
+                    "description": "A **normal** item works correctly.",
+                    "value": {
+                        "name": "Foo",
+                        "description": "A very nice Item",
+                        "price": 35.4,
+                        "tax": 3.2,
+                    },
+                },
+                "converted": {
+                    "summary": "An example with converted data",
+                    "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
+                    "value": {
+                        "name": "Bar",
+                        "price": "35.4",
+                    },
+                },
+                "invalid": {
+                    "summary": "Invalid data is rejected with an error",
+                    "value": {
+                        "name": "Baz",
+                        "price": "thirty five point four",
+                    },
+                },
+            },
+        ),
+    ],
+):
+    results = {"item_id": item_id, "item": item}
     return results
+    
